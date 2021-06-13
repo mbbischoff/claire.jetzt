96987437f7d72b67a254e8b308f3eb27879bbabe
ctypeh <code@claire.jetzt>
2021-06-13 17:17:50 -0400

    Restructuring website Moved files into simpler directories. Removed html and js functions that were unused. Properly structured and indented html. New favicon. Redesigned website theme. Again! (I have a lot of fun doing this, it's a little hobby.) Removed blurb about university.

--- js/script.js
+++ js/script.js
@@ -5,16 +5,16 @@ $(document).ready( function() {
 
 	if(hr >= 6 && hr < 12) {
 		/*Morning 6a->12p*/
-		greeting = "Good Morning";
+		greeting = "Good Morning,";
 	} else if (hr == 12) {
 		/*Noon 12pm*/
-		greeting = "Good Day";
-	} else if (hr > 12 && hr < 18) {
-		/*Afternoon 1p->6p*/
-		greeting = "Good Afternoon";
-	} else if (hr >= 18 && hr < 21) {
-		/*Evening 6p->9p*/
-		greeting = "Good Evening";
+		/*Leave Default*/
+	} else if (hr > 12 && hr < 17) {
+		/*Afternoon 1p->5p*/
+		greeting = "Good Afternoon,";
+	} else if (hr >= 17 && hr < 21) {
+		/*Evening 5p->9p*/
+		greeting = "Good Evening,";
 	} else if (hr >= 21 || hr < 6) {
 		/*Night 9p->6a*/
 		/*Leave Default*/
@@ -22,11 +22,5 @@ $(document).ready( function() {
 		/*Unknown*/
 		/*Leave Default*/
 	}
-
 	document.getElementById('greeting').innerHTML = greeting;
 });
-
-/*$(window).scroll(function(){
-	//console.log('scroll');
-	$('a').css('color', ''); //Random Color
-});*/

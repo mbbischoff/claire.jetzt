846a0d0e77ccec0a77dc84d2885f7273397234d9
Claire Violet <code@claire.jetzt>
2025-09-05 21:00:07 -0700

    cleanup comments on js

--- js/script.js
+++ js/script.js
@@ -4,23 +4,17 @@ $(document).ready( function() {
 	let hr = new Date().getHours();
 
 	if(hr >= 6 && hr < 12) {
-		/*Morning 6a->12p*/
 		greeting = "Good Morning,";
 	} else if (hr == 12) {
-		/*Noon 12pm*/
 		/*Leave Default*/
 	} else if (hr > 12 && hr < 17) {
-		/*Afternoon 1p->5p*/
 		greeting = "Good Afternoon,";
 	} else if (hr >= 17 && hr < 21) {
-		/*Evening 5p->9p*/
 		greeting = "Good Evening,";
 	} else if (hr >= 21 || hr < 6) {
-		/*Night 9p->6a*/
 		/*Leave Default*/
 	} else {
-		/*Unknown*/
-		/*Leave Default*/
+		/*Unknown, Leave Default*/
 	}
 	$('#greeting').html(greeting);
 
